425d21a85ee732dcfd589eabea7062647a56a59e
wolfieorama <wachirawamunyi@gmail.com>
2015-11-13 12:47:41 +0300

    nested forms setup

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,61 @@
 class TasksController < InheritedResources::Base
+  before_action :set_tas, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @task = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def edit
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    respond_to do |format|
+      if @task.save
+        format.html { redirect_to @task, notice: 'Task was successfuly saved. '}
+        format.json { render :show, status: :created, location: @task }
+      else
+        format.html { render :new }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @task.update
+        format.html { redirect_to @task, notice: 'Project was successfuly saved. '}
+        format.json { render :show, status: :created, location: @task }
+      else
+        format.html { render :edit }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @task.destroy
+    respond_to do |format|
+      format.html { redirect_to project_url, notice: 'Task was successfully destroyed.'}
+      format.json { head :no_content }
+    end
+  end
 
   private
+
+    def set_task
+      @task = Task.find(params[:id])
+    end
 
     def task_params
       params.require(:task).permit(:description, :done, :project_id)
     end
 end
-
